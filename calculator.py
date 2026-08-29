53654e6e3939d3de13c49ff6c81c28acdedf9418
Dhyanesh <dhyanesh0524@gmail.com>
2026-08-29 10:42:01 +0000

    Add square root operation

--- calculator.py
+++ calculator.py
@@ -1,39 +1,56 @@
 print("simple calculator")
-def add(a,b):
+
+def add(a, b):
     return a + b
-def subtract(a,b):
+
+def subtract(a, b):
     return a - b
-def multiply(a,b):
+
+def multiply(a, b):
     return a * b
-def power(a,b):
+
+def power(a, b):
     return a ** b
-def modulo(a,b):
+
+def modulo(a, b):
     return a % b
-def divide(a,b):
+
+def divide(a, b):
     if b == 0:
         return "Cannot divide by zero"
     return a / b
-operations={"+": add, "-": subtract, "*": multiply, "**": power, "%": modulo, "/": divide}     
+
+def sqrt(a):
+    return a ** 0.5
+
+operations = {"+": add, "-": subtract, "*": multiply, "**": power, "%": modulo, "/": divide}
+
 while True:
-        while True:
-            try:
-                a = float(input("Enter first number: "))
-                break
-            except ValueError:
-                print("Invalid input. Please enter a number.")
+    while True:
+        op = input("Enter operator (+, -, *, **, %, /, sqrt): ")
+        if op == "sqrt" or op in operations:
+            break
+        else:
+            print("Invalid operator. Please try again.")
+
+    while True:
+        try:
+            a = float(input("Enter first number: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    if op == "sqrt":
+        print("Result:", sqrt(a))
+    else:
         while True:
             try:
                 b = float(input("Enter second number: "))
                 break
             except ValueError:
                 print("Invalid input. Please enter a number.")
-        while True:  
-            op = input("Enter operator (+, -, *,**,%, /): ")
-            try:
-                print("Result:", operations[op](a,b))
-                break
-            except KeyError:
-                print("Invalid operator. Please try again.")
-        again = input("Do you want to perform another calculation? (y/n): ")
-        if again == "n":
-            break
+        print("Result:", operations[op](a, b))
+
+    again = input("Do you want to perform another calculation? (y/n): ")
+    if again == "n":
+        break
